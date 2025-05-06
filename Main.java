746fc396e3ed51fd66b486b542350eb8cb50df64
Felipe Matos <felipemoraesdematos@gmail.com>
2025-05-05 23:30:55 -0400

    Primeiro commit do meu projeto

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        IPhone iphone = new IPhone();
+
+        iphone.tocar();
+        iphone.selecionarMusica("Imagine - John Lennon");
+
+        iphone.ligar("123456789");
+        iphone.exibirPagina("https://www.apple.com");
+    }
+}
